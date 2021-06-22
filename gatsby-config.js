93ee8582a416303d18363090d7cd30a3ae30f3c4
hankthemason <42900752+hankthemason@users.noreply.github.com>
2021-06-22 15:47:11 -0400

    finished tutorial :

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,18 @@
 module.exports = {
   siteMetadata: {
     title: "My Portfolio",
+    description: "A static web site presenting my software development portfolio"
   },
   plugins: [
     "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-image",
-    "gatsby-plugin-sharp" 
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
   ],
 };
