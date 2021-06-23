d3e173b08e202517a30920c1aaad0bf2b4091713
hankthemason <42900752+hankthemason@users.noreply.github.com>
2021-06-23 13:10:46 -0400

    Finished making site;

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,12 +7,26 @@ module.exports = {
     "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    `gatsby-transformer-sharp`,
     {
       resolve: "gatsby-source-filesystem",
       options: {
-        name: `blog`,
-        path: `${__dirname}/blog`,
+        name: `images`,
+        path: `${__dirname}/images`,
       }
+    },
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
     },
   ],
 };
