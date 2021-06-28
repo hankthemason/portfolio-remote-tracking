cfa0580785083739236647e97d399e1b73537d0c
hankthemason <42900752+hankthemason@users.noreply.github.com>
2021-06-28 16:03:11 -0400

    updated label 'for' property to 'htmlFor' to fix errors

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -9,15 +9,15 @@ const Contact = () => {
       <form id="fs-frm" name="simple-contact-form" accept-charset="utf-8" action="https://formspree.io/f/xeqvzgqg" method="post">
         <div className={formBox}>
           <div className={child}>
-            <label for="full-name">Full Name</label>
+            <label htmlFor="full-name">Full Name</label>
             <input type="text" name="name" id="full-name" placeholder="First and Last" required="" />
           </div>
           <div className={child}>
-            <label for="email-address">Email Address</label>
+            <label htmlFor="email-address">Email Address</label>
             <input type="email" name="_replyto" id="email-address" placeholder="email@domain.tld" required="" />
           </div>
           <div className={child}>
-            <label for="message">Message</label>
+            <label htmlFor="message">Message</label>
             <textarea rows="5" name="message" id="message" placeholder="Your message here" required=""></textarea>
           </div>
           <input type="hidden" name="_subject" id="email-subject" value="Contact Form Submission" />
